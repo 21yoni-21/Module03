59a11336b0d0fdcbefa0c3eef0a553e9c1f895de
21yoni-21 <yonasgebrehans21@gmai.com>
2026-08-28 20:05:44 +0100

    Day26 Assignment in class exercise Day2

--- Day26/Addis-Eats/src/Dish.jsx
+++ Day26/Addis-Eats/src/Dish.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 function Dish({ name, price }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function handleAdd() {
+    setQuantity(quantity + 1);
+  }
+
   return (
-    <div className="dish">
-      <h2>{name}</h2>
-      <p>{price} ETB</p>
+    <div className="dish-card">
+      <h3>{name}</h3>
+
+      <p>Price: {price} ETB</p>
+
+      <button onClick={handleAdd}>
+        Add
+      </button>
+
+      <span> Quantity: {quantity}</span>
     </div>
   );
 }
 
 export default Dish;
+
---
+++ Day26/Addis-Eats/src/data.jsx
@@ -0,0 +1,55 @@
+
+const menu = [
+  {
+    id: 1,
+    name: "Doro Wat",
+    price: 240,
+    category: "Main",
+    spicy: true,
+  },
+  {
+    id: 2,
+    name: "Shiro",
+    price: 120,
+    category: "Vegetarian",
+    spicy: false,
+  },
+  {
+    id: 3,
+    name: "Kitfo",
+    price: 280,
+    category: "Main",
+    spicy: true,
+  },
+  {
+    id: 4,
+    name: "Tibs",
+    price: 220,
+    category: "Main",
+    spicy: true,
+  },
+  {
+    id: 5,
+    name: "Injera Firfir",
+    price: 150,
+    category: "Breakfast",
+    spicy: false,
+  },
+  {
+    id: 6,
+    name: "Beyaynetu",
+    price: 180,
+    category: "Vegetarian",
+    spicy: false,
+  },
+  {
+    id: 7,
+    name: "Misir Wat",
+    price: 130,
+    category: "Vegetarian",
+    spicy: true,
+  },
+];
+
+export default menu;
+
